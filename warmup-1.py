9f756029eb33610c4cb1a0167118167734b9ea18
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-09-10 14:31:46 -0500

    Solve monkey_trouble

--- warmup-1.py
+++ warmup-1.py
@@ -20,9 +20,8 @@ def monkey_trouble(a_smile, b_smile):
     monkey_trouble(False, False) → True
     monkey_trouble(True, False) → False
     """
-    return
-
-
+    return a_smile == b_smile
+     
 def sum_double(a, b):
     """
     Given two int values, return their sum. Unless the two values are the same,
