c98834bb8d542ee7bfb86ec6c3248a7a1e65d75e
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-09-10 22:40:13 -0500

    Solve squirrel_play

--- logic-1.py
+++ logic-1.py
@@ -47,7 +47,7 @@ def squirrel_play(temp, is_summer):
     squirrel_play(70, False) → True
     squirrel_play(95, False) → False
     squirrel_play(95, True) → True"""
-    return
+    return 60 <= temp <= 90 and not is_summer or 60 <= temp <= 100 and is_summer
 
 
 def caught_speeding(speed, is_birthday):
