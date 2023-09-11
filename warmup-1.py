bf9d3837ad944b87a623c9d63a0cf80cea095dd3
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-09-10 19:12:25 -0500

    Sovle front3

--- warmup-1.py
+++ warmup-1.py
@@ -127,7 +127,6 @@ def front_back(str):
     """
     return
 
-
 def front3(str):
     """
     Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
@@ -136,7 +135,7 @@ def front3(str):
     front3('Chocolate') → 'ChoChoCho'
     front3('abc') → 'abcabcabc'
     """
-    return
+    return 3*str[0:3]
 
 
 if __name__ == "__main__":
