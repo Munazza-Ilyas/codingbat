e22300ae6012e38a2402cec55e0744b623e3a099
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-09-10 22:45:30 -0500

    in1to10

--- logic-1.py
+++ logic-1.py
@@ -139,7 +139,12 @@ def in1to10(n, outside_mode):
     in1to10(5, False) → True
     in1to10(11, False) → False
     in1to10(11, True) → True"""
-    return
+    
+    if not outside_mode:
+        return 1<=n<=10
+
+    elif outside_mode:
+        return n>=10 or n<=1 
 
 
 def near_ten(num):
