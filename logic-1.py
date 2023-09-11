f56dedb5e60e50e4703e9a55b72f23ad013715b4
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-09-10 22:41:28 -0500

    Solve caught_speeding

--- logic-1.py
+++ logic-1.py
@@ -62,7 +62,22 @@ def caught_speeding(speed, is_birthday):
     caught_speeding(60, False) → 0
     caught_speeding(65, False) → 1
     caught_speeding(65, True) → 0"""
-    return
+    
+    if is_birthday:
+        if speed <= 65:
+            return 0
+        elif 66 <= speed <= 85:
+            return 1
+        elif speed >= 86:
+            return 2
+    
+    elif not is_birthday:
+        if speed <= 60:
+            return 0
+        elif 61 <= speed <= 80:
+            return 1
+        elif speed >= 81:
+            return 2
 
 
 def sorta_sum(a, b):
