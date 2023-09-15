4bc587ca6173fb16948ba4a5ecbb82884693c6aa
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-09-15 18:59:57 -0500

    Solve rotate_left3

--- list-1.py
+++ list-1.py
@@ -61,7 +61,7 @@ def rotate_left3(nums):
     rotate_left3([1, 2, 3]) → [2, 3, 1]
     rotate_left3([5, 11, 9]) → [11, 9, 5]
     rotate_left3([7, 0, 0]) → [0, 0, 7]"""
-    return
+    return [nums[1], nums[2], nums[0]]
 
 
 def reverse3(nums):
