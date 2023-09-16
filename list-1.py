31ace0f7111911c35a26f49c9c9c90cbdbb80a29
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-09-15 19:25:38 -0500

    Solve middle_way

--- list-1.py
+++ list-1.py
@@ -109,7 +109,7 @@ def middle_way(arr1, arr2):
     middle_way([1, 2, 3], [4, 5, 6]) → [2, 5]
     middle_way([7, 7, 7], [3, 8, 0]) → [7, 8]
     middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]"""
-    return
+    return [arr1[1], arr2[1]]
 
 
 def make_ends(nums):
