698549ea802323637d0f8de9e52f3358343b98d4
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-09-15 19:18:57 -0500

    Solve max_end3

--- list-1.py
+++ list-1.py
@@ -85,7 +85,7 @@ def max_end3(nums):
     max_end3([1, 2, 3]) → [3, 3, 3]
     max_end3([11, 5, 9]) → [11, 11, 11]
     max_end3([2, 11, 3]) → [3, 3, 3]"""
-    return
+    return [nums[0]]*3 if nums[0] >= nums[-1] else [nums[-1]]*3
 
 
 def sum2(nums):
