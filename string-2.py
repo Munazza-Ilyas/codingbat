d1b7701b3bfe622ec869b0a7921e8806651457e2
MUNAZZA BAWANY <munazzabawany@MUNAZZAs-Air.lan>
2023-09-16 16:47:55 -0500

    Solve double_char

--- string-2.py
+++ string-2.py
@@ -6,7 +6,11 @@ def double_char(str):
     double_char('The') → 'TThhee'
     double_char('AAbb') → 'AAAAbbbb'
     double_char('Hi-There') → 'HHii--TThheerree'"""
-    return
+    
+    new_str=""
+    for char in str:
+        new_str += 2*char
+    return new_str
 
 
 def count_hi(str):
